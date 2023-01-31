1f39ec9da912432c24e7a9e2d8a5758eada8b826
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-01-31 09:49:32 -0300

    Entities: reply, topic, deliver, notification. Fix: Missing associations, getters. Fix: Remove user roles on constructor

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Reply.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Reply.java
@@ -2,6 +2,8 @@ package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,39 +11,42 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_notification")
-public class Notification implements Serializable {
+@Table(name = "tb_reply")
+public class Reply implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String text;
+
+	@Column(columnDefinition="TEXT")	
+	private String body;
 
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant moment;
-	private boolean read;
-	private String route;
 
 	@ManyToOne
-	@JoinColumn(name = "user_id")
-	private User user;
+	@JoinColumn(name = "topic_id")
+	private Topic topic;
 
-	public Notification() {
-	}
+	@ManyToOne
+	@JoinColumn(name = "author_id")
+	private User author;
 
-	public Notification(Long id, String text, Instant moment, boolean read, String route, User user) {
-		super();
-		this.id = id;
-		this.text = text;
-		this.moment = moment;
-		this.read = read;
-		this.route = route;
-		this.user = user;
+	@ManyToMany
+	@JoinTable(name = "tb_reply_likes",
+		joinColumns = @JoinColumn(name = "reply_id"),
+		inverseJoinColumns = @JoinColumn(name = "user_id")
+	)
+	private Set<User> likes = new HashSet<>();
+
+	public Reply() {
 	}
 
 	public Long getId() {
@@ -52,12 +57,12 @@ public class Notification implements Serializable {
 		this.id = id;
 	}
 
-	public String getText() {
-		return text;
+	public String getBody() {
+		return body;
 	}
 
-	public void setText(String text) {
-		this.text = text;
+	public void setBody(String body) {
+		this.body = body;
 	}
 
 	public Instant getMoment() {
@@ -68,28 +73,24 @@ public class Notification implements Serializable {
 		this.moment = moment;
 	}
 
-	public boolean isRead() {
-		return read;
+	public Topic getTopic() {
+		return topic;
 	}
 
-	public void setRead(boolean read) {
-		this.read = read;
+	public void setTopic(Topic topic) {
+		this.topic = topic;
 	}
 
-	public String getRoute() {
-		return route;
+	public User getAuthor() {
+		return author;
 	}
 
-	public void setRoute(String route) {
-		this.route = route;
+	public void setAuthor(User author) {
+		this.author = author;
 	}
 
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
+	public Set<User> getLikes() {
+		return likes;
 	}
 
 	@Override
@@ -108,7 +109,7 @@ public class Notification implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Notification other = (Notification) obj;
+		Reply other = (Reply) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
