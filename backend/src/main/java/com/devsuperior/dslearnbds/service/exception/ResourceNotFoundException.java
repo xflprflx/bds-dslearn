85d47247f611aa07b70a8fdf0dd79db092acd53e
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-01-31 10:55:05 -0300

    Exceptions, validation, security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/service/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dslearnbds.service.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+}
