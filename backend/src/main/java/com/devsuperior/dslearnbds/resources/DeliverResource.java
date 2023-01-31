452ab85adfb6c9650db4499fdacb104e01aaf2a7
Filipe Machado <x.filipe.machado.x@gmail.com>
2023-01-31 15:31:08 -0300

    Preauthorizing methods

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/DeliverResource.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslearnbds.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslearnbds.dto.DeliveryRevisionDTO;
+import com.devsuperior.dslearnbds.services.DeliverService;
+
+@RestController
+@RequestMapping(value = "/deliveries")
+public class DeliverResource {
+
+	@Autowired
+	private DeliverService service;
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Void> saveRevision(@PathVariable Long id, @RequestBody DeliveryRevisionDTO dto) {
+		service.saveRevision(id, dto);
+		return ResponseEntity.noContent().build();
+	}
+}
